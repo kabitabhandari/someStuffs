e98204115106eb6732e834acb36a5d19ef06b35b
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2022-04-09 17:43:10 -0500

    chore:reading values from appl

---
+++ src/main/java/com/example/demo121/fakeapis/CommonConfigurationProperties.java
@@ -0,0 +1,20 @@
+package com.example.demo121.fakeapis;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@ConfigurationProperties(prefix = "kabita.common")
+@Data
+public class CommonConfigurationProperties{
+// map from application.yml kabita.common (format : smallcase, hi-phen, snakecase)
+private String best_food;
+    private String bestdrink;
+    private String bestAcharPickle;
+    private String bestcountryworld;
+    private String best_color_rainbow;
+
+}
---
+++ src/main/java/com/example/demo121/fakeapis/Testing.java
@@ -0,0 +1,29 @@
+package com.example.demo121.fakeapis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class Testing {
+    @Autowired
+    private CommonConfigurationProperties commonConfigurationProperties;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(commonConfigurationProperties);
+        String s2 = commonConfigurationProperties.getBest_food();
+        String s1 = commonConfigurationProperties.getBestdrink();
+        String s3 = commonConfigurationProperties.getBestAcharPickle();
+        String s4 = commonConfigurationProperties.getBestcountryworld();
+        String s5 = commonConfigurationProperties.getBest_color_rainbow();
+
+
+        System.out.println("food : >>> " + s2) ;
+        System.out.println("drink : >>> " + s1) ;
+        System.out.println("achar : >>> " + s3) ;
+        System.out.println("country : >>> " + s4) ;
+        System.out.println("color : >>> " + s5) ;
+    }
+}
